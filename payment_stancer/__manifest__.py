a6569dc2f29f328ee13a0c7c0688863a808a2956
Sebastien <sderen@stancer.com>
2024-10-11 12:42:25 +0200

    CHANGE: refacto of js, better use of odoo framework

--- payment_stancer/__manifest__.py
+++ payment_stancer/__manifest__.py
@@ -4,7 +4,7 @@
     "category": "Accounting/Payment Providers",
     "sequence": 350,
     "summary": "Stancer for Odoo",
-    "depends": ["payment", "account", "sale_management", "website_sale"],
+    "depends": ["payment"],
     "data": [
         "security/ir.model.access.csv",
         "wizards/process_stancer_refund_form_view.xml",
--- payment_stancer/static/src/js/stancer_iframe.js
+++ payment_stancer/static/src/js/stancer_iframe.js
@@ -1,23 +1,98 @@
 /** @odoo-module **/
-import { rpc } from '@web/core/network/rpc';
+import { rpc } from "@web/core/network/rpc";
 import PaymentForm from "@payment/js/payment_form";
 PaymentForm.include({
-  events: Object.assign({}, PaymentForm.prototype.events || {}, {
-    "click #o_payment_methods": "_onStancerClick",
-  }),
+  radioInput: document.querySelector('input[data-provider-code="stancer"]'),
+  stancerIframe: document.getElementById("stancer-iframe"),
 
-
-  init() {
-    if (
-      document.querySelector('input[data-provider-code="stancer"]') === null
-    ) {
+  /**
+   * Show or hide the iFrame depending on context
+   * @param {boolean} display
+   * @returns
+   */
+  async _displayframe(display = true) {
+    const IframeContainer = document.getElementById("stancer-checkout-iframe");
+    if (IframeContainer === null) {
       return;
     }
-    this._super(...arguments);
-    this.createIframe();
+    const PayButton = document.getElementsByName("o_payment_submit_button")[0];
+    PayButton.style.display = display ? "none" : "block";
+    IframeContainer.style.display = display ? "block" : "none";
   },
 
-  stancer_create_iframe() {
+  /**
+   * Override prepareInlineForm to use direct flow with Stancer payment in case of Iframe.
+   *
+   * @param {number} providerId
+   * @param {string} providerCode
+   * @param {number} paymentOptionId
+   * @param {string} paymentMethodCode
+   * @param {string} flow
+   * @returns void
+   */
+  async _prepareInlineForm(
+    providerId,
+    providerCode,
+    paymentOptionId,
+    paymentMethodCode,
+    flow
+  ) {
+    this._super(...arguments);
+    if (providerCode !== "stancer") {
+      this._displayframe(false);
+      return;
+    }
+    rpc("/stancer_provider_iframe_check", {
+      provider_id: providerId,
+    }).then((iframe) => {
+      if (iframe) {
+        Object.assign(this.paymentContext, {
+          tokenizationRequested: false,
+          providerId: providerId,
+          paymentMethodId: paymentOptionId,
+        });
+        this._initiatePaymentFlow(
+          providerCode,
+          paymentOptionId,
+          paymentMethodCode,
+          "direct"
+        );
+      }
+    });
+  },
+
+  /**
+   * Override
+   * Create an Iframe instead of a odoo form, leveraging our payment page.
+   *
+   * @param {number} providerId
+   * @param {string} paymentMethodCode
+   * @returns void
+   */
+  _processDirectFlow(
+    providerCode,
+    paymentOptionId,
+    paymentMethodCode,
+    processingValues
+  ) {
+    if (this.radioInput.checked === false) {
+      this._displayframe(false);
+      return;
+    }
+    this._displayframe();
+
+    if (this.stancerIframe.src.includes("about:blank")) {
+      rpc("/prepare_stancer_iframe", processingValues).then(
+        (rendering_value) => {
+          this._stancerHandleIframe(rendering_value);
+        }
+      );
+    }
+  },
+
+  /** Link the Iframe to our payment page and add a listener for the return event */
+  _stancerHandleIframe({ api_url, return_url }) {
+    this.stancerIframe.src = api_url;
     window.addEventListener("message", (e) => {
       const data = e.data;
 
@@ -25,71 +100,31 @@ PaymentForm.include({
         return;
       }
 
-      if (data.status === "finished" && data.url === null) {
-        this.stancer_redirect();
+      if (data.status === "finished") {
+        this._stancerRedirect(return_url);
         return;
       }
     });
   },
 
-  stancer_redirect() {
+  /**
+   * Create a backdrop after the 3ds redirect, making customer wait to be redirected.
+   *
+   * @param {string} return_url
+   * @returns
+   */
+  _stancerRedirect(return_url) {
     // If we have a backdrop we already are redirected.
     if (document.querySelector(".stancer-backdrop") !== null) {
       return;
     }
     const backdrop = document.createElement("div");
     const redirect_block = document.getElementById("stancer-redirect-iframe");
-    const return_url = "/payment/stancer/return";
 
     backdrop.classList.add("stancer-backdrop");
     document.body.append(backdrop);
     redirect_block.style.display = "block";
     window.location = return_url;
     backdrop.append(redirect_block);
-  },
-
-  async start() {
-    return await this._super.apply(this, arguments);
-  },
-
-  _onStancerClick(ev) {
-    this.init();
-  },
-
-  async createIframe() {
-    const IframeContainer = document.getElementById("stancer-checkout-iframe");
-    const IframeElement = document.getElementById("stancer-iframe");
-    const PayButton = document.getElementsByName("o_payment_submit_button")[0];
-    const StancerRadio = document.querySelector(
-      'input[data-provider-code="stancer"]'
-    );
-
-    if (StancerRadio.checked === true) {
-      const StancerPaymentId = parseInt(StancerRadio.dataset.providerId, 10);
-      const IsIframe = await rpc("/stancer_provider_iframe_check",
-        {
-        stancer_id: StancerPaymentId,
-      }
-    );
-
-      if (!IsIframe) {
-        return;
-      }
-
-      PayButton.style.display = "none";
-      IframeContainer.style.display = "block";
-
-      if (IframeElement.src.includes("about:blank")) {
-        IframeElement.src = await rpc("/prepare_stancer_iframe", {
-          stancer_id: StancerPaymentId,
-        });
-        this.stancer_create_iframe();
-      }
-    }
-
-    if (StancerRadio.checked === false && IframeContainer !== null) {
-      PayButton.style.display = "block";
-      IframeContainer.style.display = "none";
-    }
   },
 });
