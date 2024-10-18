b03284ff02aa12ee4d30c9300434ffbcf3c2dd3b
Sebastien <sderen@stancer.com>
2024-10-18 16:03:32 +0200

    FIX: remove account dependencies

--- payment_stancer/__manifest__.py
+++ payment_stancer/__manifest__.py
@@ -3,8 +3,8 @@
     "version": "1.0.0",
     "category": "Accounting/Payment Providers",
     "sequence": 350,
-    "summary": "Stancer for Odoo",
-    "depends": ["payment", "account"],
+    "summary": "A French payment provider covering France",
+    "depends": ["payment"],
     "data": [
         "security/ir.model.access.csv",
         "views/payment_stancer_templates.xml",
